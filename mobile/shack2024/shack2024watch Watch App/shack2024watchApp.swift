686bc7cf30e25df3c671796e69859990e2fc9e58
mgarciate <mgarciate@gmail.com>
2024-08-05 01:12:23 +0200

    Add watchos target

---
+++ mobile/shack2024/shack2024watch Watch App/shack2024watchApp.swift
@@ -0,0 +1,17 @@
+//
+//  shack2024watchApp.swift
+//  shack2024watch Watch App
+//
+//  Created by mgarciate on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct shack2024watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
