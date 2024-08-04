22096603383b522234a1f2ada543bf56f854b3da
mgarciate <mgarciate@gmail.com>
2024-08-05 01:01:18 +0200

    Create iOS project

---
+++ mobile/shack2024/shack2024/shack2024App.swift
@@ -0,0 +1,17 @@
+//
+//  shack2024App.swift
+//  shack2024
+//
+//  Created by mgarciate on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct shack2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
